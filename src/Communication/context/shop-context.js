b11b22fd8061d3572172d86f12f8d4fba720d1e0
darthsid57 <siddhantanand23@gmail.com>
2020-03-17 00:16:34 +1200

    shop-context.js

---
+++ src/Communication/context/shop-context.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default React.createContext({
+  products: [
+    { id: "p1", title: "Gaming Mouse", price: 29.99 },
+    { id: "p2", title: "Xbox Controller", price: 9.99 },
+    { id: "p3", title: "G502 Mouse", price: 0.99 },
+    { id: "p4", title: "HS50 Corsair", price: 2.99 }
+  ],
+  cart: []
+});
